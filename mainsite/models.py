23f469f1d8dfab0410bb116f88148f28987b773c
xyq946692052 <xyq2312@sina.com>
2020-06-07 11:56:36 +0800

    generate qrcode

--- mainsite/models.py
+++ mainsite/models.py
@@ -5,7 +5,8 @@ class Strpic(models.Model):
     title = models.CharField(max_length=200, verbose_name="海报内容", null=False)
     link_url = models.CharField(max_length=100, verbose_name="链接", null=True)
     link_name = models.CharField(max_length=100, verbose_name="链接名", null=True)
-    qrcode = models.ImageField(upload_to='upload', verbose_name="二维码")
+    link_qrcode = models.ImageField(upload_to='generate_qrcode', verbose_name='生成二维码', null=True)
+    qrcode = models.ImageField(upload_to='upload_qrcode', verbose_name="上传二维码", null=True)
     background = models.CharField(max_length=50, verbose_name="背景色", default="#1E90FF")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,8 @@
 
 import os
+import qrcode
 from datetime import datetime
+from django.conf import settings
 from django.shortcuts import render
 from django.shortcuts import redirect, HttpResponse,reverse
 from .models import Strpic
@@ -11,13 +13,22 @@ def index(request):
 
 
 def generate_pic(request):
+
     title = request.POST.get('title', None)
     link_url = request.POST.get('link_url', None)
     link_name = request.POST.get('link_name', None)
-    qrcode = request.FILES.get('qrcode', None)
+    upload_qrcode = request.FILES.get('qrcode', None)
     background = request.POST.get('background', None)
-    if qrcode:
-        if qrcode.name.split('.')[-1] not in ['jpeg', 'jpg', 'png']:
+
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    if link_url:
+        gen_qrcode_img = qrcode.make(link_url)
+        gen_qrcode_name = os.path.join(settings.MEDIA_ROOT, 'generate_qrcode', link_name+"_"+now).replace('\\', '/')
+        with open('%s.png' % gen_qrcode_name, 'wb') as f:
+            gen_qrcode_img.save(f)
+
+    if upload_qrcode:
+        if upload_qrcode.name.split('.')[-1] not in ['jpeg', 'jpg', 'png']:
             return HttpResponse('上传的文件必须是图片')
     params = {
         'title': title,
@@ -26,11 +37,13 @@ def generate_pic(request):
         'qrcode': qrcode,
         'background': background
     }
-    Strpic.objects.create(**params)
+    #Strpic.objects.create(**params)
     context = dict()
     context['title'] = title
     context['qrcode'] = qrcode
     context['background'] = background
     context['link_url'] = link_url
     context['link_name'] = link_name
+    if link_url:
+        context['qrcode'] = '/media/generate_qrcode/'+link_name+"_"+now+'.png'
     return render(request, 'result.html', context)
