9b0f2b186e2ca8bcf09e74c24cb4f7ea863bbf3c
xyq946692052 <xyq2312@sina.com>
2020-06-13 15:19:25 +0800

    all finished

--- mainsite/views.py
+++ mainsite/views.py
@@ -15,17 +15,11 @@ def index(request):
 
 def generate_pic(request):
 
-    title = request.POST.get('title', None)
-    link_url = request.POST.get('link_url', None)
+    title = request.POST.get('title', '地摊海报神器')
+    link_url = request.POST.get('link_url', '欢迎使用海报生成器')
     upload_qrcode = request.FILES.get('upload_qrcode', None)
     background = request.POST.get('background', None)
-
     now = datetime.now().strftime('%Y%m%d%H%M%S')
-    if link_url:
-        gen_qrcode_img = qrcode.make(link_url)
-        gen_qrcode_name = os.path.join(settings.MEDIA_ROOT, 'generate_qrcode', title[:5]+ "_"+now).replace('\\', '/')
-        with open('%s.png' % gen_qrcode_name, 'wb') as f:
-            gen_qrcode_img.save(f)
 
     if upload_qrcode:
         upload_qrcode_name = now+"_"+upload_qrcode.name
@@ -35,6 +29,11 @@ def generate_pic(request):
                 f.write(line)
         if upload_qrcode.name.split('.')[-1] not in ['jpeg', 'jpg', 'png']:
             return HttpResponse('上传的文件必须是图片')
+    else:
+        gen_qrcode_img = qrcode.make(link_url)
+        gen_qrcode_name = os.path.join(settings.MEDIA_ROOT, 'generate_qrcode', title[:5] + "_" + now).replace('\\', '/')
+        with open('%s.png' % gen_qrcode_name, 'wb') as f:
+            gen_qrcode_img.save(f)
 
     params = {
         'title': title,
@@ -46,10 +45,8 @@ def generate_pic(request):
 
     obj = Strpic.objects.all().first()
 
-
-
     context = dict()
-    context['title'] = obj.title[:45] if obj.title else None
+    context['title'] = obj.title[:45]
     context['background'] = obj.background
 
     if upload_qrcode:
@@ -57,7 +54,7 @@ def generate_pic(request):
         context['prompt'] = '长按或扫一扫进行付款'
         context['qrcode'] = path
 
-    if link_url:
+    else:
         context['link_url'] = obj.link_url[:50]
         context['prompt'] = '长按识别二维码打开链接'
         context['qrcode'] = '/media/generate_qrcode/'+obj.title[:5]+"_"+now+'.png'
